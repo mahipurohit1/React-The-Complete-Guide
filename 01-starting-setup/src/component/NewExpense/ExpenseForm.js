603cef02ff144cd48e7b6d296604c62357774d2d
mahipurohit1 <mahipurohit174@gmail.com>
2023-01-12 17:48:09 +0530

    video 67

--- 01-starting-setup/src/component/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/component/NewExpense/ExpenseForm.js
@@ -84,7 +84,11 @@ function ExpenseForm(props) {
           />
         </div>
       </div>
+
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onStopFormHandler}>
+          Cancel
+        </button>
         <button type="submit"> Add Expense</button>
       </div>
     </form>
--- 01-starting-setup/src/component/NewExpense/NewExpense.js
+++ 01-starting-setup/src/component/NewExpense/NewExpense.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 function NewExpense(props) {
+  const [editingForm, setEditingForm] = useState(false);
   function NewExpenseData(expenseData) {
     const NewExpenseData = {
       ...expenseData,
       id: Math.random().toString(),
     };
     props.onAddNewData(NewExpenseData);
+    setEditingForm(false);
   }
+  const startFormHandler = function () {
+    setEditingForm(true);
+  };
+  const stopFormHandler = function () {
+    setEditingForm(false);
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveForm={NewExpenseData} />
+      {editingForm ? (
+        <ExpenseForm
+          onSaveForm={NewExpenseData}
+          onStopFormHandler={stopFormHandler}
+        />
+      ) : (
+        <div>
+          <button onClick={startFormHandler}>Add Expense</button>
+        </div>
+      )}
     </div>
   );
 }
