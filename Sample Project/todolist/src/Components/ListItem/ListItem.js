23434abbd302f63e8696465dcb8f8ffea2783716
mahipurohit1 <mahipurohit174@gmail.com>
2023-01-19 12:59:35 +0530

    sample project done

--- Sample Project/todolist/src/Components/ListItem/ListItem.js
+++ Sample Project/todolist/src/Components/ListItem/ListItem.js
@@ -4,12 +4,18 @@ import styles from "./ListItem.module.css";
 import Task from "./Task";
 import DataContext from "../Store/DataContext";
 
-function ListItem() {
+function ListItem(props) {
   const data = useContext(DataContext);
-  const len = data.items.length;
+  const len = data.items.filter(function (item) {
+    if (props.selectOptionValue === "all") {
+      return true;
+    } else {
+      return item.status === props.selectOptionValue;
+    }
+  }).length;
   return (
     <Card>
-      <Task />
+      <Task selectOptionValue={props.selectOptionValue} />
       {!(len > 0) && <h2 className={styles["sec-heading"]}>No Item Found</h2>}
     </Card>
   );
